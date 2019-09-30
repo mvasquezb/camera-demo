4a284b9a7591706bdebc554bfee2dc1e5912d540
Martin Vasquez <mvasquez@karaokesmart.co>
2019-09-30 16:19:27 -0500

    Add saving video parts

--- app/src/main/java/com/example/camerademo/MainActivity.kt
+++ app/src/main/java/com/example/camerademo/MainActivity.kt
@@ -1,17 +1,80 @@
 package com.example.camerademo
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import androidx.lifecycle.ViewModelProvider
+import android.util.Log
+import android.view.View
+import androidx.appcompat.app.AppCompatActivity
+import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProviders
+import androidx.lifecycle.lifecycleScope
+import com.wonderkiln.camerakit.*
+import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.coroutines.launch
 
 class MainActivity : AppCompatActivity() {
 
+    companion object {
+        @JvmStatic val TAG = MainActivity::class.java.simpleName
+    }
+
+    private var isRecording = false
     private lateinit var viewModel: MainViewModel
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        setupVideoRecording()
         viewModel = ViewModelProviders.of(this).get(MainViewModel::class.java)
+        viewModel.loading.observe(this, Observer<Boolean> { loading ->
+            when (loading) {
+                true -> loadingBar.visibility = View.VISIBLE
+                else -> loadingBar.visibility = View.GONE
+            }
+        })
+    }
+
+    private fun setupVideoRecording() {
+        camera.addCameraKitListener(object : CameraKitEventListener {
+            override fun onVideo(video: CameraKitVideo?) {
+                Log.d(TAG, "video: ${video?.message}")
+            }
+
+            override fun onEvent(event: CameraKitEvent?) {
+                Log.d(TAG, "event: ${event?.message}")
+            }
+
+            override fun onImage(image: CameraKitImage?) {
+                Log.d(TAG, "image: ${image?.message}")
+            }
+
+            override fun onError(error: CameraKitError?) {
+                Log.d(TAG, "error: ${error?.message}")
+            }
+        })
+        playButton.setOnClickListener {
+            if (!isRecording) {
+                playButton.setImageDrawable(getDrawable(android.R.drawable.ic_media_pause))
+                camera.captureVideo { video ->
+                    Log.d(TAG, "video captured")
+                    lifecycleScope.launch {
+                        viewModel.handleVideo(video.videoFile)
+                    }
+                }
+            } else {
+                playButton.setImageDrawable(getDrawable(android.R.drawable.ic_media_play))
+                camera.stopVideo()
+            }
+            isRecording = !isRecording
+        }
+    }
+
+    override fun onStart() {
+        super.onStart()
+        camera.start()
+    }
+
+    override fun onStop() {
+        super.onStop()
+        camera.stop()
     }
 }
--- app/src/main/java/com/example/camerademo/MainViewModel.kt
+++ app/src/main/java/com/example/camerademo/MainViewModel.kt
@@ -1,7 +1,52 @@
 package com.example.camerademo
 
-import androidx.lifecycle.ViewModel
+import android.app.Application
+import android.os.Environment
+import androidx.lifecycle.AndroidViewModel
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.viewModelScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
+import java.io.File
 
-class MainViewModel : ViewModel() {
+class MainViewModel(app: Application) : AndroidViewModel(app) {
+    var filePath = ""
+        private set
 
+    private var numFiles = 0
+
+    val context = getApplication<Application>().applicationContext
+
+    val saveDir = context.getExternalFilesDir(Environment.DIRECTORY_DOWNLOADS)
+
+    val _loading = MutableLiveData<Boolean>()
+    val loading: LiveData<Boolean> = _loading
+
+    init {
+        _loading.value = false
+    }
+
+    suspend fun handleVideo(video: File) {
+        var savePath = "video"
+        if (numFiles != 0) {
+            savePath += "_$numFiles"
+        }
+        savePath += ".mp4"
+        if (filePath.isEmpty()) {
+            filePath = savePath
+        }
+        numFiles++
+        val outFile = File(saveDir, savePath)
+        _loading.value = true
+        viewModelScope.launch {
+            withContext(Dispatchers.IO) {
+                video.copyTo(outFile, true)
+                withContext(Dispatchers.Main) {
+                    _loading.value = false
+                }
+            }
+        }
+    }
 }

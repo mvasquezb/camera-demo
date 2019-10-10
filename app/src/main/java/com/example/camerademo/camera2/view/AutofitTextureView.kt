eb27605aa3ea0d5b219cb0d175695d48ac8181cc
Martin Vasquez <mvasquez@karaokesmart.co>
2019-10-10 16:34:44 -0500

    Modify AutofitTextureView (might be unnecessary

--- app/src/main/java/com/example/camerademo/camera2/view/AutofitTextureView.kt
+++ app/src/main/java/com/example/camerademo/camera2/view/AutofitTextureView.kt
@@ -3,14 +3,15 @@ package com.example.camerademo.camera2.view
 import android.content.Context
 import android.util.AttributeSet
 import android.view.TextureView
+import android.view.View
+import kotlin.math.abs
 
 open class AutofitTextureView @JvmOverloads constructor(
     context: Context,
     attrs: AttributeSet? = null,
     defStyle: Int = 0
 ) : TextureView(context, attrs, defStyle) {
-    private var ratioWidth = 0
-    private var ratioHeight = 0
+    private var mTargetAspect = -1.0        // initially use default window size
 
     /**
      * Sets the aspect ratio for this view. The size of the view will be measured based on the ratio
@@ -24,23 +25,58 @@ open class AutofitTextureView @JvmOverloads constructor(
         if (width < 0 || height < 0) {
             throw IllegalArgumentException("Size cannot be negative.")
         }
-        ratioWidth = width
-        ratioHeight = height
+        val newRatio = width.toDouble() / height
+        if (mTargetAspect != newRatio) {
+            mTargetAspect = newRatio
+            requestLayout()
+        }
         requestLayout()
     }
 
     override fun onMeasure(widthMeasureSpec: Int, heightMeasureSpec: Int) {
         super.onMeasure(widthMeasureSpec, heightMeasureSpec)
-        val width = MeasureSpec.getSize(widthMeasureSpec)
-        val height = MeasureSpec.getSize(heightMeasureSpec)
-        if (ratioWidth == 0 || ratioHeight == 0) {
-            setMeasuredDimension(width, height)
-        } else {
-            if (width < ((height * ratioWidth) / ratioHeight)) {
-                setMeasuredDimension(width, (width * ratioHeight) / ratioWidth)
+        var newWidthMeasureSpec = widthMeasureSpec
+        var newHeightMeasureSpec = heightMeasureSpec
+        // Target aspect ratio will be < 0 if it hasn't been set yet.  In that case,
+        // we just use whatever we've been handed.
+        if (mTargetAspect > 0) {
+            var initialWidth = MeasureSpec.getSize(widthMeasureSpec)
+            var initialHeight = MeasureSpec.getSize(heightMeasureSpec)
+
+            // factor the padding out
+            val horizPadding = paddingLeft + paddingRight
+            val vertPadding = paddingTop + paddingBottom
+            initialWidth -= horizPadding
+            initialHeight -= vertPadding
+
+            val viewAspectRatio = initialWidth.toDouble() / initialHeight
+            val aspectDiff = mTargetAspect / viewAspectRatio - 1
+
+            if (abs(aspectDiff) < 0.01) {
+                // We're very close already.  We don't want to risk switching from e.g. non-scaled
+                // 1280x720 to scaled 1280x719 because of some floating-point round-off error,
+                // so if we're really close just leave it alone.
+                /*Log.d(TAG, "aspect ratio is good (target=" + mTargetAspect +
+                        ", view=" + initialWidth + "x" + initialHeight + ")");*/
             } else {
-                setMeasuredDimension((height * ratioWidth) / ratioHeight, height)
+                if (aspectDiff > 0) {
+                    // limited by narrow width; restrict height
+                    initialHeight = (initialWidth / mTargetAspect).toInt()
+                } else {
+                    // limited by short height; restrict width
+                    initialWidth = (initialHeight * mTargetAspect).toInt()
+                }
+                /*Log.d(TAG, "new size=" + initialWidth + "x" + initialHeight + " + padding " +
+                        horizPadding + "x" + vertPadding);*/
+                initialWidth += horizPadding
+                initialHeight += vertPadding
+                newWidthMeasureSpec = MeasureSpec.makeMeasureSpec(initialWidth, MeasureSpec.EXACTLY)
+                newHeightMeasureSpec = MeasureSpec.makeMeasureSpec(initialHeight, MeasureSpec.EXACTLY)
             }
         }
+
+        //Log.d(TAG, "set width=[" + MeasureSpec.toString(widthMeasureSpec) +
+        //        "] height=[" + View.MeasureSpec.toString(heightMeasureSpec) + "]");
+        super.onMeasure(newWidthMeasureSpec, newHeightMeasureSpec)
     }
 }
